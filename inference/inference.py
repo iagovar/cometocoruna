779b7060d12f3b28a4f5872f892e3abb7588162f
iagovar <iagovar@outlook.com>
2024-01-16 17:06:54 +0100

    Flask inference server v1 done

---
+++ inference/inference.py
@@ -0,0 +1,91 @@
+# Flask importing + init
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Inference importing + init
+
+from transformers import pipeline
+
+answerer_model = "MMG/bert-base-spanish-wwm-cased-finetuned-spa-squad2-es-finetuned-sqac"
+classifier_model = "Recognai/zeroshot_selectra_medium"
+
+answerer = pipeline("question-answering", answerer_model)
+classifier = pipeline("zero-shot-classification", classifier_model)
+
+@app.route("/test", methods=["POST"])
+def test():
+    request_data = request.get_json()
+    return jsonify({"message": f"{request_data}"}), 200
+
+@app.route("/inference/qa", methods=["POST"])
+def qa():
+    """
+    Answer questions given a context.
+
+    Args:
+        context (str): The context in which the question is being asked.
+        question (str): The question being asked..
+
+    Returns:
+        str: The answer to the question.
+    """
+
+    try:
+        request_data = request.get_json()
+        context = request_data['context']
+        question = request_data['question']
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": f"Error processing request, likely failed to provide JSON payload with context and question. Flask spits: {e}"}), 500
+
+    answer = None
+
+    try:
+        answer = answerer(context=context, question=question)
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": f"Error processing request: {e}"}), 500
+
+    return jsonify({"result": answer})
+
+@app.route("/inference/categorize", methods=["POST"])
+def categorize():
+    """
+    Categorize a context into a list of categories.
+
+    Args:
+        context (str): The context in which the question is being asked.
+        categoriesList (list): The list of categories.
+
+    Returns:
+        str: A list of categories and their respective probabilities.    
+    """
+    try:
+        request_data = request.get_json()
+        context = request_data['context']
+        categoriesList = request_data['categoriesList']
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": f"Error processing request, likely failed to provide JSON payload with context and categoriesList. Flask spits: {e}"}), 500
+    
+    result = None
+
+    try:
+        answer = classifier(context, categoriesList)
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": f"Error processing request: {e}"}), 500
+
+    
+    return jsonify({"result": answer})
+
+
+@app.route("/")
+def hello():
+    return "<div>You need to specify the type of inference and input, check inference.py</div>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ inference/readme.md
@@ -0,0 +1,45 @@
+## Flask API endpoint for ML Inference
+
+While I've tried to make transformers.js work to reduce complexity surface, the ONNX library they use beneath is very problematic in model conversions. The ecosystem in JS is just not mature enough.
+
+Like it or not, the whole ML ecosystem is built upon Python, so if I don't want to call external APIs + spend money + be rate limited, I have to set up my own inference server.
+
+## How to use
+
+This server is not supossed to be exposed to the internet, as it has no authentication.
+
+Build upon python virtualenv so:
+
+1. Activate virtualenv ``source ./inference-virtualenv/bin/activate``
+2. Install the requirements ``pip install -r requirements.txt``
+3. Run server with ``flask -app inference --host:0.0.0.0``. In my case the inference server is the same hosting the scraper, there you have it in case you want to change the inference location.
+
+## How to query
+
+Look for the endpoints in the code itself, it's a single .py file.
+
+### NodeJS
+
+````
+const axios = require('axios');
+
+async function makeRequest(url, method, data) {
+ const response = await axios({
+  url: url,
+  method: method,
+  data: data
+ });
+
+ if (response.status === 200) {
+  console.log(response.data);
+ } else {
+  console.error(`Error: ${response.status}`);
+ }
+}
+
+// Usage of the function
+makeRequest('http://localhost:5000/inference/categorize', 'post', {
+ context: 'Some context',
+ categoriesList: ['category1', 'category2']
+});
+````
